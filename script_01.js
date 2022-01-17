cf7e918beed39bab3522700293f5e8b6d8286249
wahab alhamad <79885460+wahhab-alhamad@users.noreply.github.com>
2022-01-17 09:17:37 +0100

    new
    neue Zeile reingefügt

--- script_01.js
+++ script_01.js
@@ -2,6 +2,12 @@
 
 /***** Variablen 01 *******/
 
-
+console.log("Hello World");
+let firstname;
+firstname = "Wahhab ;)";
+console.log(firstname);
+let lastname;
+lastname = "Alhamad";
+console.log(firstname + " "+ lastname);
 
 
